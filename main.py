546fe71d73d423bef5e4bf9407c373da50d6548b
AkarshJ01 <akarshjaiswal02@outlook.com>
2026-05-29 03:17:12 +0400

    Started work on the reach loop

--- main.py
+++ main.py
@@ -1,45 +1,43 @@
-import os
-from langchain_core.prompts import PromptTemplate
-from langchain_ollama import ChatOllama
-
-from langfuse.langchain import CallbackHandler
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
+
+from langchain.chat_models import init_chat_model
+from langchain.tools import tools
+from langchain_core.messages import HumanMessage, SystemMessage , ToolMessage
+
+MAX_ITR = 10
+
+MODEL = "llama3.1:8b"
+
+@tools
+def get_product_price(product : str) -> float:
+    """Finding price of the product"""
+
+    print(f"Product being found : {product}")
+
+    price = {"laptop" : 1299 , "headphones" : 699 , "keyboard" : 15.9}
+
+    return price.get(product , 0)
 
 
-from langchain_tavily import TavilySearch
+@tools
+def apply_discount(price: float , discount_tier: str) -> float:
+    """Outputs the final price after discount"""
+    print (f"discount tier : {discount_tier}")
+    discount_percentage = {"bronze" : 10, "silver" : 20, "gold" : 30}
+    dis = discount_percentage.get(discount_percentage , 0)
+    price = price - price * dis /100
+    return price
 
-# @tool
-# def search(query: str) -> str:
-#     """
-#     Tool that searchs the internet
-#     Args :
-#         query : The query to search for
-#     Returns :
-#         The search result
-#     """
-#     print (f"Seach for {query}")
-#     return "Tokyo weather is great"
+# --- Agent Loop ---
 
-llm = ChatOllama(temperature=0, model="llama3.1:8b")
-tools = [TavilySearch()]
-agent = create_agent(model = llm , tools = tools)
+def run_agent(question: str):
+    pass
 
- 
-def main():
-    print("Hello from langchain!")
 
-    langfuse_handler = CallbackHandler()
 
-    result = agent.invoke({"messages" : [HumanMessage(content="What is the average package of computer science btech in nit warangal ")]},
-                          config={"callbacks": [langfuse_handler]})
-    print(result["messages"][-1].content)
-
- 
 if __name__ == "__main__":
-    main()
+    print(f"Langchain model : {MODEL} \n")
+ 
