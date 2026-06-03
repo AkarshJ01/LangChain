3e980ef4cd417f9410a1f90c98adf89fe2281a02
AkarshJ01 <akarshjaiswal02@outlook.com>
2026-06-04 00:55:18 +0400

    BoilerPlate Code for ingestion

--- ingestion.py
+++ ingestion.py
@@ -1,29 +1,33 @@
+import asyncio
 import os
-from dotenv import load_dotenv
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import CharacterTextSplitter
-from langchain_ollama import OllamaEmbeddings 
-from langchain_pinecone import PineconeVectorStore
+import ssl
+from typing import Any, List, Dict
 
+
+import certifi 
+from dotenv import load_dotenv
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_ollama import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+ 
+from logger import (Colors, log_error, log_header, log_info , log_success , log_warning)
 
 load_dotenv()
 
 
-if __name__ == "__main__":
-    print(f"Langchain model ")
+# To make sure there is no ssl error on using TAVILY
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+embeddings = OllamaEmbeddings(model = "nomic-embed-text:latest")
+
+vectorstore = PineconeVectorStore(index_name= os.environ['INDEX_NAME'], embedding=embeddings)
+tavily_extract = TavilyExtract()
+tavily_crawl = TavilyCrawl()
+tavily_map = TavilyMap(max_depth = 2, max_breadth = 20, max_pages = 1000)
 
 
-    # Loading model
-    loader = TextLoader("/Users/akarshjaiswal/Desktop/LangChain/mediumblog1.txt")
-    document = loader.load()
-    
-    #Text splitting
-    text_splitter = CharacterTextSplitter(chunk_size = 1000 , chunk_overlap = 0)
-    texts = text_splitter.split_documents(document)
-    print(f"Created {len(texts)} chunks")
-
-    #Embedding
-    embeddings = OllamaEmbeddings(model = "nomic-embed-text:latest")
-
-    #Creating a vector space
-    PineconeVectorStore.from_documents(texts, embeddings , index_name = os.environ['INDEX_NAME'])
